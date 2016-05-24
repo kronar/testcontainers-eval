a963e9f65015632fa1f3a8ce16c8a0f3becbd87e
nikita.makarov <nikita.makarov@odnoklassniki.ru>
2016-05-24 18:34:28 +0300

    Drafting twitter reporter

---
+++ src/test/java/TestAlwaysFailed.java
@@ -0,0 +1,28 @@
+import java.util.logging.Logger;
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testcontainers.containers.BrowserWebDriverContainer;
+
+/**
+ * Created by nikita on 24.05.16.
+ */
+public class TestAlwaysFailed {
+    private static final Logger LOGGER = Logger.getLogger(TestAlwaysFailed.class.getName());
+
+    @Rule
+    public BrowserWebDriverContainer chrome = new BrowserWebDriverContainer()
+            .withDesiredCapabilities(DesiredCapabilities.chrome());
+
+    @Test
+    public void test() {
+        LOGGER.info("Starting");
+        RemoteWebDriver webDriver = chrome.getWebDriver();
+        webDriver.get("https://google.com");
+
+        Assert.assertEquals(webDriver.getTitle(), "Одноклассники");
+        Screenshoter.create().takeScreenshot(webDriver);
+    }
+}

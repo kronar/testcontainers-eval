c65c8c3c53656d6aee387c3a06f9f6df7015712d
nikita.makarov <nikita.makarov@odnoklassniki.ru>
2016-05-24 18:43:48 +0300

    More on twitter

--- src/test/java/TwitterReporter.java
+++ src/test/java/TwitterReporter.java
@@ -1,14 +1,26 @@
+import com.google.common.base.Throwables;
+import java.util.logging.Logger;
 import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunListener;
+import twitter4j.Status;
 import twitter4j.Twitter;
+import twitter4j.TwitterException;
 
 /**
  */
 public class TwitterReporter extends RunListener {
 
+    private static final Logger LOGGER = Logger.getLogger(TwitterProvider.class.getName());
     @Override
     public void testFailure(Failure failure) throws Exception {
-        Twitter twitter = TwitterProvider.get();
-        twitter.updateStatus("@papaminos "+failure.getDescription().getClassName());
+        try {
+            Twitter twitter = TwitterProvider.get();
+            Status status = twitter.updateStatus("@PapaMinos " + failure.getDescription().getClassName());
+            LOGGER.info("Status "+status);
+
+        } catch (TwitterException e) {
+            LOGGER.severe(Throwables.getStackTraceAsString(e));
+
+        }
     }
 }

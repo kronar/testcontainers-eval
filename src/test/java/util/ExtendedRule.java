fca930275b21182110044efaeeab1bfe5fa0b0af
nikita.makarov <nikita.makarov@odnoklassniki.ru>
2016-05-25 10:50:33 +0300

    Additional logging

--- src/test/java/util/ExtendedRule.java
+++ src/test/java/util/ExtendedRule.java
@@ -1,12 +1,13 @@
 package util;
 
 import com.google.common.base.Throwables;
-import java.util.Map;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Optional;
 import java.util.logging.Logger;
 import org.junit.runner.Description;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testcontainers.containers.BrowserWebDriverContainer;
-import twitter4j.Status;
 import twitter4j.StatusUpdate;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -15,32 +16,35 @@ import twitter4j.TwitterException;
  */
 public class ExtendedRule extends BrowserWebDriverContainer {
     private static final Logger LOGGER = Logger.getLogger(ExtendedRule.class.getName());
+    private static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("ddMMyyyy-HHmmssSS");
 
     @Override
     protected void failed(Throwable e, Description description) {
         RemoteWebDriver webDriver = super.getWebDriver();
-        Map<String, String> getenv = System.getenv();
-        for (String s : getenv.keySet()) {
-//            if (s.startsWith("TRAVIS")) {
-            LOGGER.info("TRAVIS ENV VAR " + s + "=" + getenv.get(s));
-//            }
-        }
 
         try {
             Twitter twitter = TwitterProvider.get();
+            String travisJobNumber = Optional.ofNullable(System.getenv("TRAVIS_JOB_NUMBER")).orElse(SIMPLE_DATE_FORMAT.format(new Date()));
             String link = null;
             if (webDriver != null) {
                 link = Screenshoter.create().saveScreenshot(description.getTestClass().getName() + "_failure_screenshot", webDriver);
+                LOGGER.info("Failure Screenshot link " + link);
+            } else {
+                LOGGER.severe("Null webdriver");
+                return;
             }
 
             if (Screenshoter.NULL_OBJ.equals(link)) {
-                Status status = twitter.updateStatus("@PapaMinos " + description.getTestClass().getName());
+                LOGGER.severe("Null object screenshot");
+//                Status status = twitter.updateStatus("@PapaMinos " + description.getTestClass().getName());
             } else {
-                Status status = twitter.updateStatus("@PapaMinos " + description.getTestClass().getName());
+//                Status status = twitter.updateStatus("@PapaMinos " + description.getTestClass().getName());
 //                status = twitter.updateStatus("@PapaMinos Screenshot" + link);
 
-                StatusUpdate statusUpdate = new StatusUpdate("@PapaMinos failure screenshot at " + System.getenv("TRAVIS_BUILD_NUMBER"));
-                statusUpdate.setMedia("screenshot.png", Screenshoter.create().getScreenshotAsStream(webDriver));
+                String tweet = "@PapaMinos failure screenshot at " + travisJobNumber;
+                LOGGER.info(tweet);
+                StatusUpdate statusUpdate = new StatusUpdate(tweet);
+                statusUpdate.setMedia("screenshot" + travisJobNumber + ".png", Screenshoter.create().getScreenshotAsStream(webDriver));
                 twitter.updateStatus(statusUpdate);
             }
 

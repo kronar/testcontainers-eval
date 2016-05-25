103238c774450945308abbaf22186c4f77e40263
nikita.makarov <nikita.makarov@odnoklassniki.ru>
2016-05-25 08:55:55 +0300

    More on travis vars

--- src/test/java/util/ExtendedRule.java
+++ src/test/java/util/ExtendedRule.java
@@ -1,8 +1,10 @@
 package util;
 
 import com.google.common.base.Throwables;
+import java.util.Map;
 import org.junit.runner.Description;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.SystemClock;
 import org.testcontainers.containers.BrowserWebDriverContainer;
 import twitter4j.Status;
 import twitter4j.StatusUpdate;
@@ -28,7 +30,13 @@ public class ExtendedRule extends BrowserWebDriverContainer {
             } else {
                 Status status = twitter.updateStatus("@PapaMinos " + description.getTestClass().getName());
 //                status = twitter.updateStatus("@PapaMinos Screenshot" + link);
-                StatusUpdate statusUpdate = new StatusUpdate("@PapaMinos failure screenshot");
+                Map<String, String> getenv = System.getenv();
+                for (String s : getenv.keySet()) {
+                    if (s.startsWith("TRAVIS")){
+                        System.out.println(s+":"+getenv.get(s));
+                    }
+                }
+                StatusUpdate statusUpdate = new StatusUpdate("@PapaMinos failure screenshot at "+ System.getenv("TRAVIS_BUILD_NUMBER"));
                 statusUpdate.setMedia("screenshot.png", Screenshoter.create().getScreenshotAsStream(webDriver));
                 twitter.updateStatus(statusUpdate);
             }
